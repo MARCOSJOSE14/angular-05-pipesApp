888993cba03d0eb8dc55ca1376b50bc6301a7a67
willyctu <sistemas.corporaciontextil@gmail.com>
2024-10-06 21:46:44 -0500

    Sección 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SharedModule } from "./shared/shared.module";
+
+// Configuración de locale de la app
+import localeEsCL from "@angular/common/locales/es-CL";
+import localeFrCA from "@angular/common/locales/fr-CA";
+
+import { registerLocaleData  } from "@angular/common";
+
+registerLocaleData( localeEsCL );
+registerLocaleData( localeFrCA );
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [
+    RouterOutlet,
+    SharedModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'es-CL'
+    }
+  ]
 })
 export class AppComponent {
   public title = 'JosE mARCos rAMOs';
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
 import { ApplicationConfig } from '@angular/core';
+
+// import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    // importProvidersFrom(
+        // BrowserModule,
+        // BrowserAnimationsModule
+    // )
+  ]
 };
