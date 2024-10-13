a63e081556b17b746038157163b26fe555cf8c4a
JOSE MARCOS <joseluismarcosramos@outlook.com>
2024-10-13 01:25:21 -0500

    Pipes xd

---
+++ src/app/products/pipes/toggle-case.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+// marcos | toggleCase = 'MARCOS'
+
+@Pipe({
+  name: 'toggleCase'
+})
+export class ToggleCasePipe implements PipeTransform {
+
+  transform(value: string): string {
+    return value.toUpperCase();
+  }
+
+}
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -6,13 +6,16 @@ import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
 
 
 @NgModule({
   declarations: [
     BasicsPageComponent,
     NumbersPageComponent,
-    UncommonPageComponent
+    UncommonPageComponent,
+
+    ToggleCasePipe
   ],
   imports: [
     CommonModule,
